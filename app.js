30781896a1f2c600c9ee9e18b959163448dc95a2
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-03-05 12:50:28 +0200

    deploy

--- app.js
+++ app.js
@@ -6,27 +6,30 @@ import getAvailability from './client/src/controllers/product.js'
 
 var app = express();
 const port = process.env.PORT || 5000;
-
-app.use(cors());
-
-
-
-
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, './client/build')));
-// Handle React routing, return all requests to React app
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-});
-
-// create a router for saving the api
 const router = express.Router();
 router.get('/', async (req, res) => {
   const data = await getAvailability();
   res.json(data)
 })
 
-app.use('/api', router)
+
+
+app.use(cors());
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.use('/api', router);
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
+
+// create a router for saving the api
+
+
+
 
 // PRODUCTIOn
 // if (process.env.NODE_ENV === 'production') {
