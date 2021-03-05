eb02dd9d71a6c735e99e4637af5f828bd0fd8cc2
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-03-05 13:47:22 +0200

    deploy

--- app.js
+++ app.js
@@ -20,15 +20,16 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, './client/build')));
 
 
-function checkUserAuth(req, res, next) {
-  if (req.session.user) return next();
-  return next(new NotAuthorizedError());
-}
+let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+let workQueue = new Queue('work', REDIS_URL);
 
 app.get('/api', async (req, res) => {
+  
   const data = await getAvailability();
-  res.json(data)
-}, checkUserAuth)
+  let result = await workQueue.add();
+
+  res.json(result)
+})
 
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
@@ -49,6 +50,7 @@ app.get('*', function(req, res) {
 //     res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 //   });
 // }
+
 
 app.listen(port, () => {
   console.log(`Example app listening port ${port}`)
